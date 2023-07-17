94d20b2c9dd13a7586ab156e9e4be390611054fe
Wei Xuan <leocai@cht.com.tw>
2023-07-17 15:20:41 +0800

    feat: init project

---
+++ hello-csharp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hello_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.Read();
+            Person  p = new Person();
+            p.Name = "John";
+            p.Age = 25;
+        }
+        
+    }
+
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        
+    }
+}
